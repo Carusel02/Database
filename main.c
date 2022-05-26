a0c35709efd7445f5f26ff34449b22e4e5abb563
Marius <marinmariusdaniel02@gmail.com>
2022-05-26 18:07:05 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+
+typedef struct head
+{   unsigned char type;
+    unsigned int len; 
+} head;
+
+typedef struct data_structure
+{   head *header;
+    void *data; 
+} data_structure;
+
+int main() {
+
+
+
+
+
+    return 0;
+}
